6118d24ce93f804d3f3fdd180eb4eaaa7b1c824f
Shing Lyu <shing.lyu@booking.com>
2018-08-10 16:01:41 +0200

    Added timestamp

--- background.js
+++ background.js
@@ -7,12 +7,28 @@ chrome.contextMenus.create({
 
 });
 
+chrome.contextMenus.create({
+    id: "insert-timestamp",
+    title: "Timestamp",
+    contexts: ["editable"],
+    parentId: "insert-test-text"
+});
+
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
   switch (info.menuItemId) {
     case "insert-test-text":
+      /*
       console.debug(pending_request);
       if (typeof pending_request !== undefined) {
         pending_request({"text": "test foo"});
+      }
+      console.debug(info);
+      */
+      break;
+    case "insert-timestamp":
+      console.debug(pending_request);
+      if (typeof pending_request !== undefined) {
+        pending_request({"text": (new Date()).toISOString()});
       }
       console.debug(info);
       break;
